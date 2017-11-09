a542f032f3b5fae4710659767575a2727f6eafca
Mayerhofer Florian <mayerhfl13@r00314.fh-joanneum.at>
2017-11-09 11:35:41 +0100

    Add Coredata - not working yet

--- EmergencyAlerter/ViewController/SettingsViewController.swift
+++ EmergencyAlerter/ViewController/SettingsViewController.swift
@@ -7,16 +7,20 @@
 //
 
 import UIKit
-
+import CoreData
 
 class SettingsViewController: UIViewController {
     
     
     @IBOutlet weak var addContact: UIButton!
+    @IBOutlet weak var getContacts: UIButton!
+    
+    var people: [NSManagedObject] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+     
     }
     
     override func didReceiveMemoryWarning() {
@@ -25,7 +29,56 @@ class SettingsViewController: UIViewController {
     }
     
     @IBAction func buttonTapped(_ sender: UIButton) {
-      
+        print("Button tapped")
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        // 1
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        // 2
+        let entity =
+            NSEntityDescription.entity(forEntityName: "EmergencyContact",
+                                       in: managedContext)!
+        
+        let person = NSManagedObject(entity: entity,
+                                     insertInto: managedContext)
+        
+        // 3
+        person.setValue("Test", forKeyPath: "name")
+        person.setValue("1234", forKey: "telephoneNumber")
+        
+        // 4
+        do {
+            try managedContext.save()
+            people.append(person)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    @IBAction func getbuttonTapped(_ sender: UIButton) {
+        print(" getButton tapped")
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        // 1
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        let peopleFetch: NSFetchRequest<EmergencyContact> = EmergencyContact.fetchRequest()
+        
+        do {
+            let fetchedPeople = try managedContext.executeFetchRequest(peopleFetch) as! NSFetchRequest<EmergencyContact>
+        } catch {
+            fatalError("Failed to fetch employees: \(error)")
+        }
+
     }
 }
 
