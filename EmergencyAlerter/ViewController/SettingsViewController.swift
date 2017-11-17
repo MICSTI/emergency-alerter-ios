2f5c253051ac134162b75d98f9b3af23c11372a8
Mayerhofer Florian <mayerhfl13@r00314.fh-joanneum.at>
2017-11-17 20:11:07 +0100

    List only appending if not in it

--- EmergencyAlerter/ViewController/SettingsViewController.swift
+++ EmergencyAlerter/ViewController/SettingsViewController.swift
@@ -14,12 +14,12 @@ class SettingsViewController: UITableViewController, CNContactPickerDelegate {
     
     
     @IBOutlet weak var addContact: UIButton!
-    @IBOutlet weak var getContacts: UIButton!
     
     var people: [NSManagedObject] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        getContactsFromStore()
    
      
     }
@@ -29,10 +29,25 @@ class SettingsViewController: UITableViewController, CNContactPickerDelegate {
         // Dispose of any resources that can be recreated.
     }
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("peoples: \(people.count)")
+        return people.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "contactCell")! //1.
+        let text = people[indexPath.row].value(forKey: "name") as! String //2.
+        cell.textLabel?.text = text //3.
+        return cell //4.
+    }
+    
     @IBAction func buttonTapped(_ sender: UIButton) {
         print("Button tapped")
        
-        
         let contactPicker = CNContactPickerViewController()
         contactPicker.delegate = self
         contactPicker.displayedPropertyKeys = [CNContactGivenNameKey, CNContactPhoneNumbersKey]
@@ -67,29 +82,30 @@ class SettingsViewController: UITableViewController, CNContactPickerDelegate {
         let entity =
             NSEntityDescription.entity(forEntityName: "EmergencyContact",
                                        in: managedContext)!
-        
-        let person = NSManagedObject(entity: entity,
+        if(!contactExists(telephone: firstPhoneNumber.stringValue)){
+            let person = NSManagedObject(entity: entity,
                                      insertInto: managedContext)
         
-        // 3
-        person.setValue(userName, forKeyPath: "name")
-        person.setValue(firstPhoneNumber.stringValue, forKey: "telephoneNumber")
+            // 3
+            person.setValue(userName, forKeyPath: "name")
+            person.setValue(firstPhoneNumber.stringValue, forKey: "telephoneNumber")
         
-        // 4
-        do {
-            try managedContext.save()
-            people.append(person)
-        } catch let error as NSError {
-            print("Could not save. \(error), \(error.userInfo)")
+            // 4
+            do {
+                try managedContext.save()
+                people.append(person)
+                self.tableView.reloadData()
+            } catch let error as NSError {
+                print("Could not save. \(error), \(error.userInfo)")
+            }
         }
-        
     }
     
     func contactPickerDidCancel(_ picker: CNContactPickerViewController) {
         
     }
     
-    @IBAction func getbuttonTapped(_ sender: UIButton) {
+    func getContactsFromStore() {
         print(" getButton tapped")
        guard let appDelegate =
             UIApplication.shared.delegate as? AppDelegate else {
@@ -114,13 +130,35 @@ class SettingsViewController: UITableViewController, CNContactPickerDelegate {
             for contact in result {
                 print(contact.name!)
             }
+            people = result
             
         } catch {
             let fetchError = error as NSError
             print(fetchError)
         }
-
         
+    }
+    
+    func contactExists(telephone: String) -> Bool {
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return false
+        }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "EmergencyContact")
+        fetchRequest.predicate = NSPredicate(format: "telephoneNumber == %@", telephone)
+        fetchRequest.includesSubentities = false
+        
+        var entitiesCount = 0
+        
+        do {
+            entitiesCount = try managedContext.count(for: fetchRequest)
+        }
+        catch {
+            print("error executing fetch request: \(error)")
+        }
+        print("Exists? \(entitiesCount)")
+        return entitiesCount > 0
     }
 }
 
