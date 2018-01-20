a095569151f427b3069a5fd39217ee80900d0b1a
Mayerhofer Florian <mayerhfl13@r00314.fh-joanneum.at>
2018-01-20 13:03:31 +0100

    Added Unittest

--- EmergencyAlerterTests/EmergencyAlerterTests.swift
+++ EmergencyAlerterTests/EmergencyAlerterTests.swift
@@ -17,22 +17,42 @@ class EmergencyAlerterTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        policeLoader = PoliceLoader("https://crpladev-emal.herokuapp.com/api/nearest/police");
+        
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        policeLoader = nil;
     }
     
-    func testExample() {
-       let ex = expectation(description: "Load from inet")
+    func testCallApiSuccess() {
+       policeLoader = PoliceLoader("https://crpladev-emal.herokuapp.com/api/nearest/police");
+        let ex = expectation(description: "Load from inet")
         policeLoader.loadFromInternet(current: CurrentLocation(47.076668,15.421371)) { (polices, err) in
             XCTAssertNotNil(polices)
-            XCTAssertNil(err)
+            XCTAssertTrue(polices!.count>0)
             ex.fulfill()
         }
-        waitForExpectations(timeout: 20, handler: { (err) in
+        waitForExpectations(timeout: 10, handler: { (err) in
+            if let e = err {
+                print("Timeout with error \(e)")
+            } else {
+                print("Success")
+            }
+            
+        })
+    }
+    
+    func testCallApiNoSuccess() {
+        policeLoader = PoliceLoader("https://crpladev-emal.herokuapp.com/api/nearest/wrong");
+        let ex = expectation(description: "Load from inet")
+        policeLoader.loadFromInternet(current: CurrentLocation(47.076668,15.421371)) { (polices, err) in
+            XCTAssertNotNil(polices)
+            XCTAssertTrue(polices!.count==0)
+            ex.fulfill()
+        }
+        waitForExpectations(timeout: 10, handler: { (err) in
             if let e = err {
                 print("Timeout with error \(e)")
             } else {
